8792db3413d55137fbf1eaaa65a724de24144feb
svetline1 <sveto4ka@list.ru>
2023-10-12 21:02:59 +0300

    add  test_login_form_wrong_data

--- lesson1/test_auth.py
+++ lesson1/test_auth.py
@@ -24,6 +24,24 @@ def test_login_form():
     driver.quit()
 
 
+def test_login_form_wrong_data():
+    driver.get("https://www.saucedemo.com/")
+
+    username_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
+    username_field.send_keys("user")
+
+    password_field = driver.find_element(By.XPATH, '//input[@data-test="password"]')
+    password_field.send_keys("user")
+
+    login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
+    login_button.click()
+
+    error_button = driver.find_element(By.XPATH, '//*[@class="error-button"]')
+    assert error_button
+
+    assert driver.current_url == "https://www.saucedemo.com/"
+
+    driver.quit()
 
 
 
