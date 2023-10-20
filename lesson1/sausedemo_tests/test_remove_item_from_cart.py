47a64ab91d76fc379c5d19a988efed402332577b
svetline1 <sveto4ka@list.ru>
2023-10-20 13:37:11 +0300

    test_remove_item_from_cart.py

--- lesson1/sausedemo_tests/test_remove_item_from_cart.py
+++ lesson1/sausedemo_tests/test_remove_item_from_cart.py
@@ -1,36 +1,22 @@
-from telnetlib import EC
-
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 
-from selenium.webdriver.support.wait import WebDriverWait
 
-driver = webdriver.Chrome()
-
-def test_remove_item_from_cart():
+def test_remove_item_from_cart(driver):
     driver.get("https://www.saucedemo.com/")
 
-    username_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
-    username_field.send_keys("standard_user")
+    driver.find_element(By.XPATH, '//input[@data-test="username"]').send_keys("standard_user")
 
-    password_field = driver.find_element(By.XPATH, '//input[@data-test="password"]')
-    password_field.send_keys("secret_sauce")
+    driver.find_element(By.XPATH, '//input[@data-test="password"]').send_keys("secret_sauce")
 
-    login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
-    login_button.click()
+    driver.find_element(By.XPATH, '//input[@data-test="login-button"]').click()
 
-    button_add_to_cart = driver.find_element(By.CSS_SELECTOR, 'button[data-test="add-to-cart-sauce-labs-backpack"]')
-    button_add_to_cart.click()
+    driver.find_element(By.CSS_SELECTOR, 'button[data-test="add-to-cart-sauce-labs-backpack"]').click()
 
-    cart_button = driver.find_element(By.CSS_SELECTOR, 'a[class="shopping_cart_link"]')
-    cart_button.click()
+    driver.find_element(By.CSS_SELECTOR, 'a[class="shopping_cart_link"]').click()
 
-    remove_button = driver.find_element(By.ID, 'remove-sauce-labs-backpack')
-    remove_button.click()
+    driver.find_element(By.ID, 'remove-sauce-labs-backpack').click()
 
-    removed_cart_item = driver.find_element(By.XPATH, '//div[@class="removed_cart_item"]')
-    assert removed_cart_item
+    assert driver.find_element(By.XPATH, '//div[@class="removed_cart_item"]')
 
-    driver.quit()
+
 
