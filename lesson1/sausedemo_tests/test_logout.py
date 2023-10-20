29062b2d4c4e1eb37dfea440ee6d4532b3eb094c
svetline1 <sveto4ka@list.ru>
2023-10-20 13:33:06 +0300

    test_logout refactored

--- lesson1/sausedemo_tests/test_logout.py
+++ lesson1/sausedemo_tests/test_logout.py
@@ -1,32 +1,22 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-driver = webdriver.Chrome()
 
-
-def test_logout():
+def test_logout(driver):
     driver.get("https://www.saucedemo.com/")
 
     url_before = driver.current_url
 
-    username_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
-    username_field.send_keys("standard_user")
+    driver.find_element(By.XPATH, '//input[@data-test="username"]').send_keys("standard_user")
 
-    password_field = driver.find_element(By.XPATH, '//input[@data-test="password"]')
-    password_field.send_keys("secret_sauce")
+    driver.find_element(By.XPATH, '//input[@data-test="password"]').send_keys("secret_sauce")
 
-    login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
-    login_button.click()
+    driver.find_element(By.XPATH, '//input[@data-test="login-button"]').click()
 
-    burger_menu = driver.find_element(By.ID, 'react-burger-menu-btn')
-    burger_menu.click()
+    driver.find_element(By.ID, 'react-burger-menu-btn').click()
     time.sleep(1)
 
-    logout = driver.find_element(By.ID,'logout_sidebar_link')
-    logout.click()
+    driver.find_element(By.ID, 'logout_sidebar_link').click()
 
-    url_after = driver.current_url
-    assert url_before == url_after
+    assert url_before == driver.current_url
 
-    driver.quit()
